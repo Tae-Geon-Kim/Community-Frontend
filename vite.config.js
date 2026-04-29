51a6163fef346cba771da81c1c59303799fd9c00
Tae-Geon-Kim <danielkim3276@gmail.com>
2026-04-29 13:32:06 +0900

    refactor: 개발서버 ip 변경 반영

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     proxy: {
       // 프론트에서 '/api'로 시작하는 요청을 보내면, 아래 target 주소로
       '/api': {
-        target: 'http://192.168.0.60', // 실제 백엔드 주소
+        target: 'http://192.168.0.246', // 실제 백엔드 주소
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') 
       }
